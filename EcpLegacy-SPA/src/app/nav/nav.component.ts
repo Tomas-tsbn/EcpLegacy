128b07329f132fddbf9645b792d4f3b50cd21d40
Tomas B <toben.gbg@gmail.com>
2019-01-13 16:31:54 +0100

    Added ngx-bootstrap and bootswtach to get some bootstrap themes

--- EcpLegacy-SPA/src/app/nav/nav.component.ts
+++ EcpLegacy-SPA/src/app/nav/nav.component.ts
@@ -28,6 +28,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    this.alertify.message('logged out');
+    this.alertify.warning('logged out');
   }
 }
